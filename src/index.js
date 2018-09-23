161ec403a38a40870a32b90136d44b1eb92dbaa7
Yauheni Payuchenka <payuchenka.e@gmail.com>
2018-09-23 20:56:27 +0300

    +

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+ 
+  var triangles = 0;
+    for (var i = 0; i < preferences.length-2; i++){
+    
+    var firstSpichonee = preferences [i];
+    var secondSpichonee = preferences [firstSpichonee-1];
+    var thirdSpichonee = preferences [secondSpichonee-1];
+    if (thirdSpichonee == i+1 && firstSpichonee > i+1 && secondSpichonee > i+1){
+       triangles++;
+    }
+  }
+  return triangles;
+  
 };
+
